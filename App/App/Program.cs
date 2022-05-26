cd70f2cdb3e07c7a852bf2b515df4af56d3d13df
elvinthy <106176744+elvinthy@users.noreply.github.com>
2022-05-26 21:48:03 +0400

    Added some files

---
+++ App/App/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace App
+{
+    class Program
+    {
+        static void Main(string[] args)
+       {
+            int elvinYear = 19;
+            int jeyhunYear = 21;
+            int elnurYear = 24;
+
+            int totalAge = elvinYear + elnurYear + jeyhunYear;
+
+            float test = totalAge / 4;
+
+            bool doTheyFriends = true;
+
+            int multiplication = elvinYear * elnurYear;
+
+            string elvinAge = "19";
+
+
+
+
+
+
+
+
+
+            Console.WriteLine((elvinAge));
+            Console.WriteLine((multiplication = elvinYear * elnurYear));
+            Console.WriteLine((elvinYear + elnurYear + jeyhunYear) / 4);
+            Console.WriteLine((doTheyFriends));
+
+
+
+
+        }
+    }       
+
+}
